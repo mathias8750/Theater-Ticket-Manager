236d3c90fe54d04e32c48a1d5c21341e6d15e8b7
mathias8750 <80708990+mathias8750@users.noreply.github.com>
2022-09-19 22:06:40 -0500

    Replaced SQLite with Supabase
    After more trying I have Supabase seemingly working with the app now.

--- src/renderer/login/view/Login.js
+++ src/renderer/login/view/Login.js
@@ -1,9 +1,23 @@
 import '../css/Login.css'
 import {Card, CardContent, Grid} from "@mui/material";
 import {useState} from "react";
+import {createClient} from '@supabase/supabase-js';
 
+const supabase = createClient(
+  'https://szqedkllaitqpzsbbmcs.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6cWVka2xsYWl0cXB6c2JibWNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjE1NDUwODYsImV4cCI6MTk3NzEyMTA4Nn0.1M2TsD1QrqN4Dnk5JauSkrpO408JcrU1dDJcniaIcPA'
+)
+
+// print data from Venues table to console
+async function getVenue() {
+  let{data, error} = await supabase.from('Venues').select('*');
+  console.log(data);
+}
+getVenue();
 
 const Login = ({}) => {
+
+  
 
   const [toggle, setToggle] = useState(false)
 
