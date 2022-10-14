8e3186d998727a26414fd4aebd34cbc8dd3db117
JasonZ21 <94938391+JasonZ21@users.noreply.github.com>
2022-10-13 21:03:22 -0500

    Revert "Revert "Merge branch 'andrew-dev' into jason-dev""
    This reverts commit 6fdf7ac32d0ad2f2bed5305cf70b80d3d452041c.

--- src/renderer/pages/customerHome/CustomerHome.js
+++ src/renderer/pages/customerHome/CustomerHome.js
@@ -1,23 +1,41 @@
-import {Button, Typography} from "@mui/material";
+import {AppBar, Button, Toolbar, Typography} from "@mui/material";
 import {Link as NavLink} from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+
+
+import "./style.css";
+
 
 
 const CustomerHome = ({}) => {
 
   return (
     <>
-      <Typography variant="h3" >Customer Home Page</Typography>
+      <AppBar className="topbar"  >
+        <Toolbar>
+          <Typography variant="h6" className="title"  >Theater Ticket Manager</Typography>
+          <Typography variant="subtitle1" className="title" >Home</Typography>
+          <NavLink to={"/employee/login"}>
+            <Button className="button">
+              Employee Login
+            </Button>
+          </NavLink>
+        </Toolbar>
+      </AppBar>
+      
+      <TextField
+          id="standard-search"
+          label="Search field"
+          type="search"
+          variant="standard"
+        />
 
       <NavLink to={"/customer/events"}>
         <Button>
-          Customer Events
+          See All Events
         </Button>
       </NavLink>
-      <NavLink to={"/employee/login"}>
-        <Button>
-          Employee Login
-        </Button>
-      </NavLink>
+
     </>
   )
 }
