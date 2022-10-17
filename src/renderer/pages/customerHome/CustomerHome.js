1fe2a1460131734bdf2ee69f974aae31f1cbe043
mewoocat <azure.horizon.4@gmail.com>
2022-10-17 06:38:53 -0500

    Added events list

--- src/renderer/pages/customerHome/CustomerHome.js
+++ src/renderer/pages/customerHome/CustomerHome.js
@@ -1,7 +1,13 @@
-import {Button, Typography} from "@mui/material";
+import {Button, Typography, Box, Grid} from "@mui/material";
 import {Link as NavLink} from 'react-router-dom';
 import CustomerHeader from "../../components/CustomerHeader";
 import TextField from '@mui/material/TextField';
+import ScrollableSidebar from "../../components/ScrollableSidebar";
+import supabase from "../../utils/Supabase";
+import {useQuery} from "@tanstack/react-query";
+import {useState} from "react";
+import CustomerEvent from "../customerEvents/components/CustomerEvent";
+
 
 
 import "./style.css";
@@ -9,6 +15,30 @@ import "./style.css";
 
 
 const CustomerHome = ({}) => {
+
+  const [selectedEvent, setSelectedEvent] = useState(null)
+
+  const fetchEvents = async () => {
+    const { data: events } = await supabase
+      .from('Events')
+      .select('*')
+
+    return events;
+  }
+
+  const {status, data, error} = useQuery(['events'], fetchEvents)
+
+  if (status === 'loading') {
+    return <span>Loading...</span>
+  }
+
+  if (status === 'error') {
+    return <span>Error: {error.message}</span>
+  }
+
+  const onEventClick = (event) => {
+    setSelectedEvent(event)
+  }
 
   return (
     <CustomerHeader>
@@ -20,6 +50,7 @@ const CustomerHome = ({}) => {
           type="search"
           variant="standard"
         />
+        
       <NavLink to={"/customer/events"}>
         <Button>
           See All Events
@@ -30,6 +61,23 @@ const CustomerHome = ({}) => {
           Employee Login
         </Button>
       </NavLink>
+
+      <Box style={{ flexGrow: 1, background: 'white', height: '100%'}}>
+        <Grid container style={{padding: '10px', height: '100%'}}>
+          <Grid item md={4} style={{paddingRight: '10px', height: '100%'}}>
+            <ScrollableSidebar events={data} onEventClick={onEventClick}/>
+          </Grid>
+
+
+          <Grid item md={8} style={{paddingRight: '10px', height: '100%'}}>
+            {selectedEvent !== null ? (
+              <CustomerEvent event={selectedEvent}/>
+            ) : (
+              <></>
+            )}
+          </Grid>
+        </Grid>
+      </Box>
     </CustomerHeader>
   )
 }
