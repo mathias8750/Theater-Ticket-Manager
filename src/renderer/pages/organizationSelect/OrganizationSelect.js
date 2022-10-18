a3e2005d9445045215c9f8dd29334f0f1d5fe48b
JasonZ21 <94938391+JasonZ21@users.noreply.github.com>
2022-10-17 20:37:49 -0500

    Revert "Revert "Merge branch 'matt-dev' into jason-dev""
    This reverts commit 53c588d3c943a3b5d005eead7ea164c05c58fdc4.

--- src/renderer/pages/organizationSelect/OrganizationSelect.js
+++ src/renderer/pages/organizationSelect/OrganizationSelect.js
@@ -1,24 +1,55 @@
-import {Button, Typography, FormControl, InputLabel, Select, MenuItem} from "@mui/material";
-import {Link as NavLink} from "react-router-dom";
+import {Button, Typography, FormControl, InputLabel, Select, MenuItem, Alert, AlertTitle, Dialog} from "@mui/material";
+import {Link as NavLink, useNavigate} from "react-router-dom";
 import OrganizationList from "../../components/OrganizationList";
+import { OrganizationContext } from "renderer/context/OrganizationContext";
+import React, {useRef, useState, useContext} from "react";
 import React from "react";
 
 
 const OrganizationSelect = ({}) => {
+
+  const {state} = useContext(OrganizationContext);
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+
+  // If org isn't selected alert the user, if it is navigate to employee home page
+  const onButton = () => {
+    if(state.selectedOrg.organizationID == 0){
+        toggleAlert();
+    } else {
+      navigate("/employee/home");
+    }
+  }
+
+  // Function to toggle the alert message
+  const toggleAlert = () => {
+    setOpen(!open);
+  }
 
   return (
     <>
 
       <Typography>Organization Select Page</Typography>
 
-      
       <OrganizationList/>
 
-      <NavLink to={"/employee/home"}>
-        <Button>
-          Employee Home
-        </Button>
-      </NavLink>
+      <Button
+      variant='contained'
+      color='primary'
+      size='small'
+      onClick={onButton}
+      >
+        Select Organization
+      </Button>
+
+      <Dialog open={open} onClose={toggleAlert}>
+        <Alert
+        severity="info"
+        >
+          <AlertTitle>Alert</AlertTitle>
+          Please select an organization
+          </Alert>
+      </Dialog>
     </>
   )
 }
