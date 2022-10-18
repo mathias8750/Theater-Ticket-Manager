f69574f2955362f758a253359c993444087688e4
JasonZ21 <94938391+JasonZ21@users.noreply.github.com>
2022-10-17 20:08:00 -0500

    .

--- src/renderer/components/EmployeeHeader.js
+++ src/renderer/components/EmployeeHeader.js
@@ -1,35 +1,35 @@
 import {AppBar, Box, Button, Grid, TextField, Toolbar} from "@mui/material";
 import SidebarEventItem from "./SidebarEventItem";
-import {useNavigate} from "react-router-dom";
+import {Link as NavLink} from "react-router-dom";
 
 
 const EmployeeHeader = (props) => {
-
-    let navigate = useNavigate();
 
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static" style={{ background: 'gray', borderShadow: 'none'}}>
           <Toolbar style={{ height: '10%'}}>
-            <Button
-                color="inherit"
-                onClick={() => navigate("/employee/home")}>
+            <NavLink to={"/employee/home"}>
+              <Button
+                  color="inherit">
                     Employee Home
-            </Button>
+              </Button>
+            </NavLink>
             <div style={{ flexGrow: 1}}/>
-
-            <Button
+            <NavLink>
+              <Button
                 color="inherit"
-                style={{alignItems: 'right'}}
-            >
+                style={{alignItems: 'right'}}>
                     Help
-            </Button>
-            <Button
-                color="inherit"
-                onClick={() => navigate("/")}>
+              </Button>
+            </NavLink>
+            <NavLink to={"/"}>
+              <Button
+                color="inherit">
                     Logout
-            </Button>
+              </Button>
+            </NavLink>
           </Toolbar>
         </AppBar>
       </Box>
