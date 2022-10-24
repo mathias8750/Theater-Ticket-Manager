fe07bc7b7411e91c03f57d2a8bff0d4d498b36e9
JasonZ21 <94938391+JasonZ21@users.noreply.github.com>
2022-10-24 15:04:54 -0500

    .

--- src/renderer/pages/employeeEvents/EmployeeEvents.js
+++ src/renderer/pages/employeeEvents/EmployeeEvents.js
@@ -1,13 +1,17 @@
-import {Typography, Button} from "@mui/material";
+import {Box, Card, CardContent, Grid, Typography, Button} from "@mui/material";
+import ScrollableSidebar from "../../components/ScrollableSidebar";
 import {Link as NavLink} from "react-router-dom";
 import EmployeeHeader from "../../components/EmployeeHeader"; 
+import supabase from "../../utils/Supabase";
+import {useQuery} from "@tanstack/react-query";
+import {useState} from "react";
 
-const EmployeeEvents = ({}) => {
+const EmployeeEvents = ({event}) => {
 
     return (
         <>
           <EmployeeHeader/>
-
+          
           
         </>
       )
