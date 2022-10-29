9485533aa9b6de6f6d02fea5d9b0af71a93788ad
mathias8750 <80708990+mathias8750@users.noreply.github.com>
2022-10-29 15:44:25 -0500

    Home page events now sorted
    The customer home page now displays the 4 closest upcoming events

--- src/renderer/pages/customerHome/CustomerHome.js
+++ src/renderer/pages/customerHome/CustomerHome.js
@@ -5,7 +5,7 @@ import TextField from '@mui/material/TextField';
 import ScrollableSidebar from "../../components/ScrollableSidebar";
 import supabase from "../../utils/Supabase";
 import {useQuery} from "@tanstack/react-query";
-import {useState} from "react";
+import React, {useState} from "react";
 import CustomerEvent from "../customerEvents/components/CustomerEvent";
 
 
@@ -17,13 +17,42 @@ import { autoUpdater } from "electron";
 
 const CustomerHome = ({}) => {
 
+  // Number of upcoming events to display
+  const numEvents = 4;
+
+  const currentDateTime = new Date();
   const [selectedEvent, setSelectedEvent] = useState(null)
+
+  // Compares the datetime of two event objects; used for array sort
+  const compareDateTime = (a,b) => {
+    let aDateTime = new Date(a.eventDateTime);
+    let bDateTime = new Date(b.eventDateTime);
+
+    if (aDateTime > bDateTime) {
+      return 1;
+    } else {
+      return -1;
+    }
+  }
+
+  // Returns array of the closest upcoming events
+  const getUpcomingEvents = (events) => {
+    let upcomingEvents = [];
+    for (let i = 0; i < events.length; i++) {
+      let eventDateTime = new Date(events[i].eventDateTime);
+      if (eventDateTime > currentDateTime) {
+        upcomingEvents = [events[i], ...upcomingEvents];
+      }
+    }
+    upcomingEvents.sort(compareDateTime);
+    const closestUpcomingEvents = upcomingEvents.slice(0, numEvents);
+    return closestUpcomingEvents;
+  }
 
   const fetchEvents = async () => {
     const { data: events } = await supabase
       .from('Events')
-      .select('*, Organizations(organizationName), Venues(venueName)')
-      .range(0, 3);
+      .select('*, Organizations(organizationName), Venues(venueName)');
 
     return events;
   }
@@ -45,10 +74,11 @@ const CustomerHome = ({}) => {
   return (
     <CustomerHeader>
       <Typography variant="h6" align="center" style={{padding: '10px'}}>Upcoming Events</Typography>
+      
       <Box style={{ flexGrow: 1, background: 'white', height: '90%'}}>
         <Grid  style={{padding: '10px', margin: 'auto', width: '80%'}}>
           <Grid item md={4} style={{paddingRight: '0px', height: '100%'}}>
-            <ScrollableSidebar events={data} onEventClick={onEventClick}/>
+            <ScrollableSidebar events={getUpcomingEvents(data)} onEventClick={onEventClick}/>
           </Grid>
         </Grid>
       </Box>
