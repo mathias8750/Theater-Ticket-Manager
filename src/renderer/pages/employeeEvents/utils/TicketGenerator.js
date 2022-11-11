3b4f98e68ccfa1f6bda3545d569b0c975de704ae
mathias8750 <80708990+mathias8750@users.noreply.github.com>
2022-11-11 17:20:00 -0600

    Default ticket prices now generated
    Ticket generator now calculates and sets the default ticket prices for the playhouse

--- src/renderer/pages/employeeEvents/utils/TicketGenerator.js
+++ src/renderer/pages/employeeEvents/utils/TicketGenerator.js
@@ -4,21 +4,37 @@
     To generate set of tickets for an event,
     call the 'generateTickets(event)' function
 
-    NOTE: Right now this still leaves the initial
-          price for all the seats at $0.00, and
-          it only works for events at the playhouse.
+    NOTE: Right now this only works for the playhouse,
+          and it doesn't mark season ticket holders'
+          seats as sold
 
 */
 
+import { useState } from "react";
 import { PlayhouseLoges, PlayhouseSections } from "renderer/data/PlayhouseSeatMapJson";
 import supabase from "renderer/utils/Supabase";
+
+// Event org object
+let eventOrg = {};
 
 // Function to add array of tickets to the tickets table in supabase
 const insertTickets = async (tickets) => {
     const {data, error} = await supabase
       .from('Tickets')
       .insert(tickets); 
-} 
+}
+
+// Function to get an event's organization object data from supabase
+const fetchOrg = async (event) => {
+    const {data, error} = await supabase
+      .from('Organizations')
+      .select('*')
+      .eq('organizationID', event.organizationID)
+
+    if (!error) {
+        eventOrg = data[0];
+    }
+}
 
 // Function to generate the initial tickets for an event and add them to the supabase
 export const generateTickets = (event) => {
@@ -34,8 +50,8 @@ export const generateTickets = (event) => {
             Object.entries(section[1]).map(row => {
                 if(row[0] != 'data') {
                     Object.entries(row[1]).map(seat => {
-                        // TODO: set default seat price here with seat[1].price
-                        tickets.push({soldBool: false, priceValue: seat[1].price, eventID: event.eventID, seasonID: event.seasonID, seatNumber: seat[0], rowNumber: row[0], sectionNumber: 'S' + section[0]});
+                        // TODO: if seat belongs to season ticket holder, mark it sold
+                        tickets.push({soldBool: false, priceValue: 0.0, eventID: event.eventID, seasonID: event.seasonID, seatNumber: seat[0], rowNumber: row[0], sectionNumber: section[0]});
                     })
                 }
             })
@@ -46,15 +62,47 @@ export const generateTickets = (event) => {
             Object.entries(section[1]).map(row => {
                 if(row[0] != 'data') {
                     Object.entries(row[1]).map(seat => {
-                        // TODO: set default seat price here with seat[1].price
-                        tickets.push({soldBool: false, priceValue: seat[1].price, eventID: event.eventID, seasonID: event.seasonID, seatNumber: seat[0], rowNumber: row[0], sectionNumber: 'L' + section[0]});
+                        // TODO: if seat belongs to season ticket holder, mark it sold
+                        tickets.push({soldBool: false, priceValue: 0.0, eventID: event.eventID, seasonID: event.seasonID, seatNumber: seat[0], rowNumber: row[0], sectionNumber: section[0]});
                     })
                 }
             })
-        })
-
+        })   
     }
 
-    // insert the new tickets into the supabase
-    insertTickets(tickets);
+    // TODO: create tickets for concert hall
+    if (event.venueID === 1) {
+        
+    }
+
+    // calculate ticket prices based on org's min and max default prices
+    fetchOrg(event).then(() => {
+        if (event.venueID === 2) {
+            let diff = (eventOrg.organizationMaxPrice - eventOrg.organizationMinPrice) / 9;
+            for (const ticket of tickets) {
+                if (ticket.sectionNumber === 'S1' || ticket.sectionNumber === 'S2' || ticket.sectionNumber === 'S3' || ticket.sectionNumber === 'S4') {
+                    for (let row = 'A'.charCodeAt(0); row <= 'G'.charCodeAt(0); row++) {
+                        if (ticket.rowNumber.charCodeAt(0) === row) {
+                            ticket.priceValue = parseFloat((eventOrg.organizationMaxPrice - (((row-'A'.charCodeAt(0))) * diff)).toFixed(2));
+                        }
+                    }
+                }
+                if (ticket.sectionNumber === 'L1' || ticket.sectionNumber === 'L2' || ticket.sectionNumber === 'L3' || ticket.sectionNumber === 'L4') {
+                    for (let row = 'A'.charCodeAt(0); row <= 'C'.charCodeAt(0); row++) {
+                        if (ticket.rowNumber.charCodeAt(0) === row) {
+                            ticket.priceValue = parseFloat((eventOrg.organizationMaxPrice - (((row-'A'.charCodeAt(0))+7) * diff)).toFixed(2));
+                        }
+                    }
+                }
+            }
+        }
+        if (event.venueID === 1) {
+            // TODO: calculate ticket prices for events at concert hall
+        }
+
+        // insert the new tickets into the supabase
+        insertTickets(tickets);
+    });
+
+    console.log(tickets);
 }
