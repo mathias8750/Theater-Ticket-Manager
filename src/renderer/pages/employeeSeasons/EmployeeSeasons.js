e6fb4263b21231c44b0592affe82bd2de4cc5482
mewoocat <azure.horizon.4@gmail.com>
2022-11-19 18:24:43 -0600

    Update EmployeeSeasons.js
    Add season sorting

--- src/renderer/pages/employeeSeasons/EmployeeSeasons.js
+++ src/renderer/pages/employeeSeasons/EmployeeSeasons.js
@@ -10,11 +10,65 @@ import {useQuery} from "@tanstack/react-query";
 import {useContext, useState} from "react";
 import Season from "./components/Season";
 import { OrganizationContext } from "renderer/context/Context";
+import { compareDateTime } from "renderer/utils/DateTime";
+
 
 const EmployeeSeasons = ({}) => {
 
   const [selectedSeason, setselectedSeason] = useState(null)
   const {state} = useContext(OrganizationContext)
+
+const sortSeasons = (seasons) => {
+	if (seasons.length == 0){
+		console.log("No seasons");
+		return seasons;
+	}
+	let sortedSeasons = [];
+	let ignore = [];
+	// make smallest the index
+	//let smallestDate = new Date(seasons[0].startDate);
+	let smallestSeason;
+	let smallestIndex;
+	for (let i = 0; i < seasons.length; i++){
+		//let smallestDate = new Date(seasons[i].startDate);
+		//let smallestSeason = seasons[i];
+		//let smallestIndex = i;
+		let smallestDate = new Date(2050, 11, 24, 10, 33, 30, 0);
+		console.log(i);
+		for (let j = 0; j < seasons.length; j++){
+			if (ignore.includes(j) == false){
+				d = new Date(seasons[j].startDate);
+				if (d < smallestDate){
+					smallestDate = d;
+					smallestSeason = seasons[j];
+					smallestIndex = j;
+				}
+			}
+		}
+		ignore.push(smallestIndex)
+		sortedSeasons.push(smallestSeason);
+	}
+	console.log(ignore)
+  
+  return sortedSeasons;
+}
+
+  // Sort seasons by date
+  // Adapted from CustomerHome.js
+  const currentDateTime = new Date();
+  const sortSeasonsBackup = (seasons) => {
+    //sortedSeasons = seasons;
+    let sortedSeasons = [];
+    
+    for (let i = 0; i < seasons.length; i++) {
+      let seasonDate = new Date(seasons[i].startDate);
+      if (seasonDate > currentDateTime) {
+        sortedSeasons = [seasons[i], ...sortedSeasons];
+      }
+    }
+    sortedSeasons.sort(compareDateTime);
+    return sortedSeasons;
+  }
 
   const fetchSeasons = async () => {
     const { data: seasons } = await supabase
@@ -23,12 +77,27 @@ const EmployeeSeasons = ({}) => {
       .from('Seasons')
       .select('*')
       .eq('organizationID', state.selectedOrg.organizationID)
-
-
+/*  Attempt at sorting seasons by date
+    seasons = seasons.sort(function(a, b){
+      return new Date(b.startDate) - new Date(a.startDate);
+    });
+*/
     return seasons;
   }
 
+
+
   const {status, data, error} = useQuery(['seasons'], fetchSeasons)
+
+  /*
+  data.sort(function(d1, d2){
+    return d1.date - d2.date;
+  });
+  */
+ //data.sort((a, b) => a.startDate - b.startDate);
+ console.log(data)
+ 
+
 
   if (status === 'loading') {
     return <span>Loading...</span>
@@ -48,7 +117,7 @@ const EmployeeSeasons = ({}) => {
         <Box style={{ flexGrow: 1, background: 'white', height: '100%'}}>
         <Grid container style={{padding: '10px', height: '100%'}}>
           <Grid item md={4} style={{paddingRight: '10px', height: '100%'}}>
-            <ScrollableSidebar seasons={data} onSeasonClick={onEventClick}/>
+            <ScrollableSidebar seasons={sortSeasons(data)} onSeasonClick={onEventClick}/>
           </Grid>
 
 
