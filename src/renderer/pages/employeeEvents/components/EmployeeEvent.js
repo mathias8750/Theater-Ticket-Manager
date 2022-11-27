c10f6a8af2b0f0f355358f67944ac37c75890a2d
JasonZ21 <94938391+JasonZ21@users.noreply.github.com>
2022-11-27 13:48:06 -0600

    .

--- src/renderer/pages/employeeEvents/components/EmployeeEvent.js
+++ src/renderer/pages/employeeEvents/components/EmployeeEvent.js
@@ -1,13 +1,38 @@
 import React, { useState } from 'react';
-import { Typography, Button } from '@mui/material';
+import { Typography, Button, Card, CardHeader} from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import CustomerEvents from 'renderer/pages/customerEvents/CustomerEvents';
+import supabase from 'renderer/utils/Supabase';
+import {useQuery} from "@tanstack/react-query";
 
 const EmployeeEvent = ({event}) => {
 
     const navigate = useNavigate();
 
+    const [eventTickets, setEventTickets] = useState([]);
+
     const editPrices = () => {
         navigate("/employee/home/events/ticket-price-manager", {state: event});
+    }
+
+    const fetchTickets = async () => {
+      let {data: tickets, error} = await supabase
+        .from('Tickets')
+        .select('*')
+        .eq('eventID', event.eventID)
+  
+        setEventTickets(tickets);
+        return tickets;
+    }
+
+    const {status, data, error} = useQuery(['tickets'], fetchTickets)
+
+    if (status === 'loading') {
+      return <span>Loading...</span>
+    }
+
+    if (status === 'error') {
+      return <span>Error: {error.message}</span>
     }
 
     return (
@@ -27,6 +52,19 @@ const EmployeeEvent = ({event}) => {
         >
           Edit Ticket Prices
         </Button>
+    <Typography>List of Customers for Event
+       {eventTickets.map((ticket) => {
+            if (ticket.eventID === event.eventID) {
+              <Card>
+                <CardHeader
+                  title={ticket.customerID}
+                  subtitle={ticket.customerID}
+                />
+              </Card>
+            }
+        })}
+    </Typography>
+    
         </div>
         </>
     )
